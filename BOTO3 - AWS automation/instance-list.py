e4665b4e53ac5e9b3a338f3cf25c15b84110c5f8
Suraj SK <64256552+sksuraj17@users.noreply.github.com>
2023-08-02 15:37:51 +0530

    Delete BOTO3 - AWS automation directory

--- BOTO3 - AWS automation/instance-list.py
+++
@@ -1,9 +0,0 @@
-import boto3
-from pprint import pprint
-aws_management_console = boto3.Session(profile_name="default")
-ec2_console = aws_management_console.client(service_name="ec2")
-
-request = ec2_console.describe_instances()["Reservations"]
-for value in request:
-    for instance in value["Instances"]:
-        pprint(instance["InstanceId"])
